7e486d75af3ba259ccaeca397719c5229b811c78
Lele Lew <lele@lelelew.com>
2021-02-09 23:27:29 -0800

    add message entity

---
+++ server/entities/message.ts
@@ -0,0 +1,61 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import Adapters from "next-auth/adapters";
+// const typeorm = require("typeorm");
+// const { Entity, PrimaryGeneratedColumn, Column } = typeorm;
+
+@Entity("messages")
+export class Message extends Adapters.TypeORM.Models.Message.model {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "bigint",
+    name: "user_id",
+  })
+  userId: number;
+
+  @Column({
+    type: "varchar",
+  })
+  text: string;
+
+  @Column({
+    type: "date",
+  })
+  date: string;
+
+  @Column({
+    type: "smallint",
+  })
+  time: number;
+
+  @Column({
+    type: "boolean",
+  })
+  sent: boolean;
+
+  @Column({
+    type: "timestamp",
+    name: "created_at",
+  })
+  createdAt: string;
+
+  @Column({
+    type: "timestamp",
+    name: "updated_at",
+  })
+  updatedAt: string;
+}
+
+export const MessageSchema = {
+  name: "Message",
+  target: Message,
+  columns: {
+    ...Adapters.TypeORM.Models.Message.schema.columns,
+    // Adds a phoneNumber to the User schema
+    // phoneNumber: {
+    //   type: "varchar",
+    //   nullable: true,
+    // },
+  },
+};
--- server/lib/connect-to-database.ts
+++ server/lib/connect-to-database.ts
@@ -1,22 +1,19 @@
 import "reflect-metadata";
-import {createConnection, Connection, getConnectionOptions} from "typeorm";
+import { createConnection, Connection, getConnectionOptions } from "typeorm";
 import { User } from "../entities/user";
-
-
+import { Message } from "../entities/message";
 
 // here createConnection will load connection options from
 // ormconfig.json / ormconfig.js / ormconfig.yml / ormconfig.env / ormconfig.xml
 // files, or from special environment variables
 
-export async function connectToDatabase () {
+export async function connectToDatabase() {
   const connectionOptions = await getConnectionOptions();
   const options: any = {
     ...connectionOptions,
-    entities: [User]
-  }
+    entities: [User, Message],
+  };
   const connection: Connection = await createConnection(options);
 
   return connection;
-  
-  
 }
