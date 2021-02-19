9c4e861b79efe2e9f82b329975ba018bc73d2702
Lele Lew <lele@lelelew.com>
2021-02-18 23:51:52 -0800

    create a client side service to create new message

---
+++ client/services/message-service.ts
@@ -0,0 +1,27 @@
+/**
+ * Make a request to the API to create a message to be sent
+ * @param text message to be sent
+ * @param date date that message should be sent
+ * @param time time that message should be sent
+
+ *
+ * @returns {boolean} true if successful, false otherwise
+ */
+
+export async function createMessage(text, date, time): Promise<boolean> {
+  try {
+    const response = await fetch("/api/messages/create-message", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text, date, time }),
+    });
+    let result = await response.json();
+    return result;
+  } catch (e) {
+    console.log("error creating message");
+    return false;
+  }
+  return true;
+}
