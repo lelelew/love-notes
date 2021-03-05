333f73543290ff10576cae519d67b38559d1adff
Lele Lew <lele@lelelew.com>
2021-03-04 23:44:47 -0800

    create server side message service to list messages

--- server/services/message-service.ts
+++ server/services/message-service.ts
@@ -1,4 +1,4 @@
-import { getRepository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import { Message } from "../entities/message";
 
 export async function createMessage(
@@ -16,4 +16,10 @@ export async function createMessage(
     time,
   };
   return await messageRepository.save(message);
+}
+
+export async function listMessages(userId: string) {
+  const messageRepository = getRepository(Message); // you can also get it via getConnection().getRepository() or getManager().getRepository()
+  const userMessages = await messageRepository.find({ userId: userId });
+  return userMessages;
 }
